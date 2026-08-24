cb29514158e4fb6e9ed16a2382a9d803988e1e88
ekaf08 <105408310+ekaf08@users.noreply.github.com>
2026-08-24 09:20:24 +0700

    Implement role seeder to populate roles from JSON file

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RoleSeeder extends Seeder
 {
@@ -12,6 +13,17 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $json = file_get_contents(database_path('json/roles.json'));
+        $roles = json_decode($json, true);
+
+        foreach ($roles as $role) {
+            DB::table('roles')->insert([
+                'name'       => $role['name'],
+                'level'      => $role['level'],
+                'guard_name' => $role['guard_name'],
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
