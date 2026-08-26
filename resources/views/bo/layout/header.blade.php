de30722a3389713c8e5df22de753fb8b9d6b48cf
ekaf08 <105408310+ekaf08@users.noreply.github.com>
2026-08-26 09:49:41 +0700

    Update logout link to use JavaScript for improved functionality

--- resources/views/bo/layout/header.blade.php
+++ resources/views/bo/layout/header.blade.php
@@ -1379,7 +1379,7 @@
 									<!--end::Menu item-->
 									<!--begin::Menu item-->
 									<div class="menu-item px-5">
-										<a href="authentication/layouts/corporate/sign-in.html" class="menu-link px-5">Sign Out</a>
+										<a href="javascript:void(0)" class="menu-link px-5" id="logoutBtn">Sign Out</a>
 									</div>
 									<!--end::Menu item-->
 								</div>
