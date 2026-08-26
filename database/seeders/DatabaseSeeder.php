e55574bcd0f1361e6e1f637975a5720ad3c76a98
ekaf08 <105408310+ekaf08@users.noreply.github.com>
2026-08-26 10:55:16 +0700

    Add MKecSeeder and MKelSeeder to DatabaseSeeder for improved data seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             UserSeeder::class,
+            MKecSeeder::class,
+            MKelSeeder::class,
         ]);
         User::factory()->create([
             'name' => 'Test User',
