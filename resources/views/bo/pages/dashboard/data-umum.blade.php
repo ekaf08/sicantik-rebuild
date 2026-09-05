ed183699b5e84c6c7baac652fb2b6606dbeb67b7
MeyiFreva <rvgmzz011@gmail.com>
2026-09-05 12:00:48 +0700

    feat: bagi dashboard

---
+++ resources/views/bo/pages/dashboard/data-umum.blade.php
@@ -0,0 +1,129 @@
+<!--begin::Table Container-->
+<div class="table-responsive">
+    <table class="table table-bordered table-striped align-middle gs-0 gy-4 text-center">
+        <!--begin:mana:Table head-->
+        <thead>
+            <tr class="fw-bold fs-7 text-uppercase bg-light align-middle text-center">
+                <th rowspan="3" class="w-50px bg-dark text-white">NO</th>
+                <th rowspan="3" class="min-w-150px bg-secondary text-dark">Nama Kecamatan</th>
+                <th colspan="2" class="bg-danger text-white">Jumlah TP. PKK</th>
+                <th rowspan="3" class="w-60px bg-secondary text-dark">RW</th>
+                <th colspan="4" class="bg-primary text-white">Jumlah Kelompok</th>
+                <th colspan="2" class="bg-success text-white">Jumlah</th>
+                <th colspan="2" class="bg-info text-white">Jumlah Jiwa</th>
+                <th colspan="6" class="bg-warning text-white">Jumlah Kader</th>
+                <th colspan="4" class="bg-pink text-white">Jumlah Tenaga Sekretariat</th>
+                <th rowspan="3" class="w-100px bg-secondary text-dark">Keterangan</th>
+            </tr>
+            <tr class="fw-bold fs-7 text-uppercase align-middle text-center">
+                <th rowspan="2" class="bg-light-danger">Desa</th>
+                <th rowspan="2" class="bg-light-danger">Kelurahan</th>
+                <th rowspan="2" class="bg-light-primary">Dusun / Lingkungan</th>
+                <th rowspan="2" class="bg-light-primary">PKK RW</th>
+                <th rowspan="2" class="bg-light-primary">PKK RT</th>
+                <th rowspan="2" class="bg-light-primary">Dasa Wisma</th>
+                <th rowspan="2" class="bg-light-success">KRT</th>
+                <th rowspan="2" class="bg-light-success">KK</th>
+                <th rowspan="2" class="bg-light-info">L</th>
+                <th rowspan="2" class="bg-light-info">P</th>
+                <th colspan="2" class="bg-light-warning">Anggota TP PKK</th>
+                <th colspan="2" class="bg-light-warning">Umum</th>
+                <th colspan="2" class="bg-light-warning">Khusus</th>
+                <th colspan="2" class="bg-light-pink">Honorer</th>
+                <th colspan="2" class="bg-light-pink">Bantuan</th>
+            </tr>
+            <tr class="fw-bold fs-7 text-uppercase text-center">
+                <th class="bg-light-warning">L</th>
+                <th class="bg-light-warning">P</th>
+                <th class="bg-light-warning">L</th>
+                <th class="bg-light-warning">P</th>
+                <th class="bg-light-warning">L</th>
+                <th class="bg-light-warning">P</th>
+                <th class="bg-light-pink">L</th>
+                <th class="bg-light-pink">P</th>
+                <th class="bg-light-pink">L</th>
+                <th class="bg-light-pink">P</th>
+            </tr>
+        </thead>
+        <!--end::Table head-->
+        <!--begin::Table body-->
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td class="text-start fw-bold">Tegalsari</td>
+                <td>0</td>
+                <td>6</td>
+                <td>45</td>
+                <td>45</td>
+                <td>189</td>
+                <td>135</td>
+                <td>18</td>
+                <td>1.245</td>
+                <td>1.087</td>
+                <td>23.456</td>
+                <td>24.112</td>
+                <td>125</td>
+                <td>135</td>
+                <td>32</td>
+                <td>28</td>
+                <td>1.087</td>
+                <td>23.456</td>
+                <td>24.112</td>
+                <td>125</td>
+                <td>135</td>
+                <td>32</td>
+                <td>28</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td class="text-start fw-bold">Genteng</td>
+                <td>0</td>
+                <td>7</td>
+                <td>62</td>
+                <td>62</td>
+                <td>256</td>
+                <td>184</td>
+                <td>22</td>
+                <td>1.376</td>
+                <td>1.198</td>
+                <td>25.332</td>
+                <td>25.821</td>
+                <td>132</td>
+                <td>142</td>
+                <td>35</td>
+                <td>30</td>
+                <td>1.087</td>
+                <td>23.456</td>
+                <td>24.112</td>
+                <td>125</td>
+                <td>135</td>
+                <td>32</td>
+                <td>28</td>
+            </tr>
+        </tbody>
+        <!--end::Table body-->
+        <!--begin::Table foot (Total)-->
+        <tfoot>
+            <tr class="fw-bold fs-6 bg-light text-dark">
+                <td colspan="2" class="text-center">TOTAL</td>
+                <td>0</td>
+                <td>42</td>
+                <td>342</td>
+                <td>342</td>
+                <td>342</td>
+                <td>1.393</td>
+                <td>984</td>
+                <td>8.030</td>
+                <td>7.003</td>
+                <td>146.280</td>
+                <td>149.647</td>
+                <td>769</td>
+                <td>827</td>
+                <td>200</td>
+                <td>175</td>
+            </tr>
+        </tfoot>
+        <!--end::Table foot-->
+    </table>
+</div>
+<!--end::Table Container-->
