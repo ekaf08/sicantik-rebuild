b5397713522d84d386ea074377461fc582a7f4eb
Annisa Azzahra <greengorilla712@gmail.com>
2026-09-09 07:58:49 +0700

    feat: pokja 3 dan pokja 4

---
+++ resources/views/bo/pages/dashboard/pokja-3.blade.php
@@ -0,0 +1,148 @@
+<div class="table-responsive">
+    <table class="table table-bordered table-striped align-middle gs-0 gy-4 text-center">
+        <thead>
+            <tr class="fw-bold fs-7 text-uppercase align-middle text-center">
+                <th rowspan="3" class="w-50px bg-dark text-white">NO</th>
+                <th rowspan="3" class="min-w-150px bg-secondary text-dark">NAMA WILAYAH</th>
+                <th colspan="3" class="bg-primary text-white">JUMLAH KADER</th>
+                <th colspan="9" class="bg-success text-white">PANGAN</th>
+                <th colspan="3" class="bg-info text-white">JUMLAH INDUSTRI RUMAH TANGGA</th>
+                <th colspan="2" class="bg-warning text-white">JUMLAH RUMAH</th>
+            </tr>
+            <tr class="fw-bold fs-7 text-uppercase align-middle text-center">
+                <th rowspan="2" class="bg-light-primary">PANGAN</th>
+                <th rowspan="2" class="bg-light-primary">SANDANG</th>
+                <th rowspan="2" class="bg-light-primary">TATA LAKSANA RUMAH TANGGA</th>
+                <th colspan="2" class="bg-light-success">MAKANAN POKOK</th>
+                <th colspan="7" class="bg-light">PEMANFAATAN PEKARANGAN / HATINYA PKK</th>
+                <th rowspan="2" class="bg-light-info">PANGAN</th>
+                <th rowspan="2" class="bg-light-info">SANDANG</th>
+                <th rowspan="2" class="bg-light-info">JASA</th>
+                <th rowspan="2" class="bg-light-warning">SEHAT DAN LAYAK HUNI</th>
+                <th rowspan="2" class="bg-light-warning">TIDAK SEHAT DAN TIDAK LAYAK HUNI</th>
+            </tr>
+            <tr class="fw-bold fs-7 text-uppercase align-middle text-center">
+                <th class="bg-light-success">BERAS</th>
+                <th class="bg-light-success">NON BERAS</th>
+                <th class="bg-light">PETERNAKAN</th>
+                <th class="bg-light">PERIKANAN</th>
+                <th class="bg-light">WARUNG HIDUP</th>
+                <th class="bg-light">LUMBUNG HIDUP</th>
+                <th class="bg-light">TOGA</th>
+                <th class="bg-light">TANAMAN KERAS</th>
+                <th class="bg-light">TANAMAN LAINNYA</th>
+            </tr>
+            <tr class="fw-semibold fs-8 text-gray-500 bg-light text-center">
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+                <td>7</td>
+                <td>8</td>
+                <td>9</td>
+                <td>10</td>
+                <td>11</td>
+                <td>12</td>
+                <td>13</td>
+                <td>14</td>
+                <td>15</td>
+                <td>16</td>
+                <td>17</td>
+                <td>18</td>
+                <td>19</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td class="text-start fw-bold">ASEMROWO</td>
+                <td class="text-primary fw-bold">19</td>
+                <td class="text-primary fw-bold">19</td>
+                <td class="text-primary fw-bold">19</td>
+                <td class="text-primary fw-bold">8.622</td>
+                <td>0</td>
+                <td class="text-primary fw-bold">737</td>
+                <td class="text-primary fw-bold">2</td>
+                <td class="text-primary fw-bold">62</td>
+                <td>0</td>
+                <td class="text-primary fw-bold">2.203</td>
+                <td class="text-primary fw-bold">878</td>
+                <td>0</td>
+                <td class="text-primary fw-bold">114</td>
+                <td class="text-primary fw-bold">2</td>
+                <td class="text-primary fw-bold">7</td>
+                <td class="text-primary fw-bold">5.505</td>
+                <td class="text-primary fw-bold">64</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td class="text-start fw-bold">BENOWO</td>
+                <td class="text-primary fw-bold">21</td>
+                <td class="text-primary fw-bold">21</td>
+                <td class="text-primary fw-bold">21</td>
+                <td class="text-primary fw-bold">13.631</td>
+                <td>0</td>
+                <td class="text-primary fw-bold">40</td>
+                <td class="text-primary fw-bold">17</td>
+                <td class="text-primary fw-bold">5.635</td>
+                <td class="text-primary fw-bold">7</td>
+                <td class="text-primary fw-bold">6.327</td>
+                <td class="text-primary fw-bold">186</td>
+                <td>0</td>
+                <td class="text-primary fw-bold">90</td>
+                <td class="text-primary fw-bold">570</td>
+                <td class="text-primary fw-bold">356</td>
+                <td class="text-primary fw-bold">825</td>
+                <td class="text-primary fw-bold">4</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td class="text-start fw-bold">BUBUTAN</td>
+                <td class="text-primary fw-bold">35</td>
+                <td class="text-primary fw-bold">35</td>
+                <td class="text-primary fw-bold">35</td>
+                <td class="text-primary fw-bold">5.469</td>
+                <td>0</td>
+                <td class="text-primary fw-bold">90</td>
+                <td class="text-primary fw-bold">2</td>
+                <td class="text-primary fw-bold">82</td>
+                <td class="text-primary fw-bold">33</td>
+                <td class="text-primary fw-bold">5.729</td>
+                <td class="text-primary fw-bold">4.338</td>
+                <td>0</td>
+                <td class="text-primary fw-bold">99</td>
+                <td class="text-primary fw-bold">71</td>
+                <td class="text-primary fw-bold">62</td>
+                <td class="text-primary fw-bold">4.551</td>
+                <td class="text-primary fw-bold">168</td>
+            </tr>
+        </tbody>
+        <!--end::Table body-->
+
+        <!--begin::Table foot-->
+        <tfoot>
+            <tr class="fw-bold fs-6 bg-light text-dark">
+                <td colspan="2" class="text-center">TOTAL</td>
+                <td class="text-primary">75</td>
+                <td class="text-primary">75</td>
+                <td class="text-primary">75</td>
+                <td class="text-primary">27.722</td>
+                <td>0</td>
+                <td class="text-primary">867</td>
+                <td class="text-primary">21</td>
+                <td class="text-primary">5.779</td>
+                <td class="text-primary">40</td>
+                <td class="text-primary">14.259</td>
+                <td class="text-primary">5.402</td>
+                <td>0</td>
+                <td class="text-primary">303</td>
+                <td class="text-primary">643</td>
+                <td class="text-primary">425</td>
+                <td class="text-primary">10.881</td>
+                <td class="text-primary">236</td>
+            </tr>
+        </tfoot>
+    </table>
+</div>
---
+++ resources/views/bo/pages/dashboard/pokja-4.blade.php
@@ -0,0 +1,223 @@
+<!--begin::Table Container-->
+<div class="table-responsive">
+    <table class="table table-bordered table-striped align-middle gs-0 gy-4 text-center">
+        <!--begin::Table head-->
+        <thead>
+            <!-- Header Level 1 -->
+            <tr class="fw-bold fs-7 text-uppercase align-middle text-center">
+                <th rowspan="3" class="w-50px bg-dark text-white">NO</th>
+                <th rowspan="3" class="min-w-150px bg-secondary text-dark">NAMA WILAYAH</th>
+                <th colspan="10" class="bg-success text-white">KESEHATAN</th>
+                <th colspan="7" class="bg-info text-white">KELESTARIAN LINGKUNGAN HIDUP</th>
+                <th colspan="6" class="bg-warning text-white">PERENCANAAN SEHAT</th>
+                <th colspan="3" class="bg-primary text-white">PROGRAM UNGGULAN GERAKAN KELUARGA SEHAT TANGGUH BENCANA (GKSTB)</th>
+            </tr>
+            <!-- Header Level 2 -->
+            <tr class="fw-bold fs-7 text-uppercase align-middle text-center">
+                <!-- Sub KESEHATAN -->
+                <th colspan="6" class="bg-light-success text-success">JUMLAH KADER</th>
+                <th rowspan="2" class="bg-light-success text-success">POSYANDU</th>
+                <th rowspan="2" class="bg-light-success text-success">IMUNISASI / VAKSINASI BAYI/BALITA</th>
+                <th rowspan="2" class="bg-light-success text-success">PKG</th>
+                <th rowspan="2" class="bg-light-success text-success">TBC</th>
+                <!-- Sub KELESTARIAN LINGKUNGAN HIDUP -->
+                <th colspan="3" class="bg-light-info text-info">JUMLAH RUMAH YANG MEMILIKI</th>
+                <th rowspan="2">MCK</th>
+                <th colspan="3" class="bg-light-info text-info">JUMLAH KRT YANG MENGGUNAKAN AIR</th>
+                <!-- Sub PERENCANAAN SEHAT -->
+                <th rowspan="2" class="bg-light-warning text-warning">JUMLAH PUS</th>
+                <th rowspan="2" class="bg-light-warning text-warning">JUMLAH WUS</th>
+                <th colspan="2" class="bg-light-warning text-warning">JUMLAH AKSEPTOR KB</th>
+                <th rowspan="2" class="bg-light-warning text-warning">JML. KK YANG MEMILIKI TABUNGAN KELUARGA</th>
+                <th rowspan="2" class="bg-light-warning text-warning">JML. KK YANG MEMILIKI ASURANSI KESEHATAN</th>
+                <!-- Sub PROGRAM UNGGULAN GKSTB -->
+                <th rowspan="2" class="bg-light-primary text-primary">KESEHATAN</th>
+                <th rowspan="2" class="bg-light-primary text-primary">KELESTARIAN LINGKUNGAN HIDUP</th>
+                <th rowspan="2" class="bg-light-primary text-primary">PERENCANAAN SEHAT</th>
+            </tr>
+            <!-- Header Level 3 -->
+            <tr class="fw-bold fs-7 text-uppercase align-middle text-center">
+                <!-- Detail JUMLAH KADER -->
+                <th class="bg-light">KADER KESEHATAN</th>
+                <th class="bg-light">GIZI</th>
+                <th class="bg-light">KESLING</th>
+                <th class="bg-light">PHBS</th>
+                <th class="bg-light">KB</th>
+                <th class="bg-light">KADER SURABAYA HEBAT</th>
+                <!-- Detail JUMLAH RUMAH YANG MEMILIKI -->
+                <th class="bg-light">JAMBAN</th>
+                <th class="bg-light">SPAL</th>
+                <th class="bg-light">TEMPAT SAMPAH</th>
+                <!-- Detail JUMLAH KRT YANG MENGGUNAKAN AIR -->
+                <th class="bg-light">PDAM</th>
+                <th class="bg-light">SUMUR</th>
+                <th class="bg-light">LAIN-LAIN</th>
+                <!-- Detail JUMLAH AKSEPTOR KB -->
+                <th class="bg-light">L</th>
+                <th class="bg-light">P</th>
+            </tr>
+            <!-- Header Level 4 (Penomoran Kolom 1-28) -->
+            <tr class="fw-semibold fs-8 text-gray-500 bg-light text-center">
+                <td>1</td>
+                <td>2</td>
+                <td>3</td>
+                <td>4</td>
+                <td>5</td>
+                <td>6</td>
+                <td>7</td>
+                <td>8</td>
+                <td>9</td>
+                <td>10</td>
+                <td>11</td>
+                <td>12</td>
+                <td>13</td>
+                <td>14</td>
+                <td>15</td>
+                <td>16</td>
+                <td>17</td>
+                <td>18</td>
+                <td>19</td>
+                <td>20</td>
+                <td>21</td>
+                <td>22</td>
+                <td>23</td>
+                <td>24</td>
+                <td>25</td>
+                <td>26</td>
+                <td>27</td>
+                <td>28</td>
+            </tr>
+        </thead>
+        <!--end::Table head-->
+
+        <!--begin::Table body-->
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td class="text-start fw-bold">ASEMROWO</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td class="text-primary fw-bold">39</td>
+                <td class="fw-bold">501</td>
+                <td class="fw-bold">0</td>
+                <td class="text-primary fw-bold">0</td>
+                <td class="text-primary fw-bold">0</td>
+                <td class="text-primary fw-bold">0</td>
+                <td class="text-primary fw-bold">5.892</td>
+                <td class="text-primary fw-bold">1.170</td>
+                <td class="text-primary fw-bold">5.845</td>
+                <td class="text-primary fw-bold">0</td>
+                <td class="text-primary fw-bold">1.271</td>
+                <td class="text-primary fw-bold">81</td>
+                <td class="text-primary fw-bold">0</td>
+                <td class="fw-bold">8.484</td>
+                <td class="fw-bold">15.197</td>
+                <td class="fw-bold">23</td>
+                <td class="fw-bold">4.035</td>
+                <td class="fw-bold">970</td>
+                <td class="fw-bold">0</td>
+                <td class="text-primary fw-bold">0</td>
+                <td class="text-primary fw-bold">0</td>
+                <td class="text-primary fw-bold">0</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td class="text-start fw-bold">BENOWO</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td class="text-primary fw-bold">65</td>
+                <td class="fw-bold">707</td>
+                <td class="fw-bold">0</td>
+                <td class="text-primary fw-bold">0</td>
+                <td class="text-primary fw-bold">0</td>
+                <td class="text-primary fw-bold">0</td>
+                <td class="text-primary fw-bold">777</td>
+                <td class="text-primary fw-bold">5.772</td>
+                <td class="text-primary fw-bold">777</td>
+                <td class="text-primary fw-bold">0</td>
+                <td class="text-primary fw-bold">1.036</td>
+                <td class="text-primary fw-bold">6</td>
+                <td class="text-primary fw-bold">0</td>
+                <td class="fw-bold">13.257</td>
+                <td class="fw-bold">23.359</td>
+                <td class="fw-bold">306</td>
+                <td class="fw-bold">7.925</td>
+                <td class="fw-bold">782</td>
+                <td class="fw-bold">0</td>
+                <td class="text-primary fw-bold">5</td>
+                <td class="text-primary fw-bold">4</td>
+                <td class="text-primary fw-bold">0</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td class="text-start fw-bold">BUBUTAN</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td class="text-primary fw-bold">92</td>
+                <td class="fw-bold">972</td>
+                <td class="fw-bold">0</td>
+                <td class="text-primary fw-bold">0</td>
+                <td class="text-primary fw-bold">0</td>
+                <td class="text-primary fw-bold">0</td>
+                <td class="text-primary fw-bold">1.536</td>
+                <td class="text-primary fw-bold">1.554</td>
+                <td class="text-primary fw-bold">2.757</td>
+                <td class="text-primary fw-bold">8</td>
+                <td class="text-primary fw-bold">2.575</td>
+                <td class="text-primary fw-bold">98</td>
+                <td class="text-primary fw-bold">0</td>
+                <td class="fw-bold">13.285</td>
+                <td class="fw-bold">26.535</td>
+                <td class="fw-bold">1.122</td>
+                <td class="fw-bold">12.017</td>
+                <td class="fw-bold">160</td>
+                <td class="fw-bold">0</td>
+                <td class="text-primary fw-bold">0</td>
+                <td class="text-primary fw-bold">0</td>
+                <td class="text-primary fw-bold">0</td>
+            </tr>
+        </tbody>
+        <!--end::Table body-->
+
+        <!--begin::Table foot-->
+        <tfoot>
+            <tr class="fw-bold fs-6 bg-light text-dark">
+                <td colspan="2" class="text-center">TOTAL</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td class="text-primary">196</td>
+                <td>2.180</td>
+                <td>0</td>
+                <td class="text-primary">0</td>
+                <td class="text-primary">0</td>
+                <td class="text-primary">0</td>
+                <td class="text-primary">8.205</td>
+                <td class="text-primary">8.496</td>
+                <td class="text-primary">9.379</td>
+                <td class="text-primary">8</td>
+                <td class="text-primary">4.882</td>
+                <td class="text-primary">185</td>
+                <td class="text-primary">0</td>
+                <td>35.026</td>
+                <td>65.091</td>
+                <td>1.451</td>
+                <td>23.977</td>
+                <td>1.912</td>
+                <td>0</td>
+                <td class="text-primary">5</td>
+                <td class="text-primary">4</td>
+                <td class="text-primary">0</td>
+            </tr>
+        </tfoot>
+        <!--end::Table foot-->
+    </table>
+</div>
+<!--end::Table Container-->
